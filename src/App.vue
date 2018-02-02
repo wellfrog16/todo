ce0a4717b10859516ce35d9e98d683938b095fa7
wellfrog <wellfrog16@gmail.com>
2018-02-02 17:48:38 +0800

    开发更新

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <template>
     <div id="app">
         <div class="container">
-            <header><input type="text" placeholder="请输入待办事项"></header>
+            <header><input type="text" placeholder="请输入待办事项" v-model="todo" @keyup.enter="save()"></header>
             <section>
                 <ul>
-                    <li>
+                    <li v-for="item in list" v-bind:key="item.id">
                         <span class="finish">完成</span>
-                        <div>11111</div>
-                        <span class="delete">删除</span>
-                    </li>
-                    <li class="finished">
-                        <span class="finish">完成</span>
-                        <div>22222</div>
-                        <span class="delete">删除</span>
-                    </li>
-                    <li>
-                        <span class="finish">完成</span>
-                        <div>33333</div>
-                        <span class="delete">删除</span>
-                    </li>
-                    <li>
-                        <span class="finish">完成</span>
-                        <div>44444</div>
+                        <div>{{ item.todo }}</div>
                         <span class="delete">删除</span>
                     </li>
                 </ul>
@@ -36,24 +21,99 @@ import Hammer from 'hammer';
 
 export default {
     name: 'app',
+    data() {
+        return {
+            todo: '',
+            list: []
+        };
+    },
     mounted() {
-        const li = $('li>div');
-        li.each((index, item) => {
-            this.addHandle(item);
-        });
+        $('li>div').each((index, item) => this.addHandle(item));
     },
     methods: {
+        save() {
+            if (!this.todo) { return; }
+            this.list.unshift({ todo: this.todo, id: new Date().getTime() });
+            this.todo = '';
+
+            this.$nextTick(() => this.addHandle($('li:first-child>div')[0]));
+        },
         addHandle(item) {
+            item.status = 0;    // 初始状态
+
             let mc = new Hammer.Manager(item);
-            let Pan = new Hammer.Pan();
+            let Pan = new Hammer.Pan({
+                threshold: 0,
+                direction: Hammer.DIRECTION_HORIZONTAL  // 仅水平方向
+            });
+
+            const spanWidth = $(item).parent().find('span.delete').width();
+            let itemLeft = 0;
+            let additionalEvent = '';
 
             mc.add(Pan);
-            mc.on('panleft', function(e) {
-                // 删除按钮宽度
-                // const spanWidth = $(item).parent().find('span.delete').width();
+            mc.on('panstart', function(e) {
+                itemLeft = parseInt($(item).css('left'));
+            });
+            mc.on('panmove', function(e) {
+                additionalEvent = e.additionalEvent;
 
-                $(item).css({'left': e.deltaX + 'px'});
-                console.log(e.deltaX);
+                // 默认状态时，左滑，打开删除按钮
+                if (e.additionalEvent === 'panleft' && item.status === 0) {
+                    if (Math.abs(itemLeft + e.deltaX) >= spanWidth) { return; }
+                    $(item).css({'left': (itemLeft + e.deltaX) + 'px'});
+                }
+
+                // 默认状态时，右滑，滑动到底完成，期间显示完成提示
+                if (e.additionalEvent === 'panright' && item.status === 0) {
+                    if (itemLeft + e.deltaX >= spanWidth) { return; }
+                    $(item).css({'left': (itemLeft + e.deltaX) + 'px'});
+                }
+
+                // 状态1，删除按钮打开时，右滑，关闭删除按钮
+                if (e.additionalEvent === 'panright' && item.status === 1) {
+                    if (itemLeft + e.deltaX >= 0) { return; }
+                    $(item).css({'left': (itemLeft + e.deltaX) + 'px'});
+                }
+            });
+            mc.on('panend', function(e) {
+                itemLeft = parseInt($(item).css('left'));
+
+                console.log(item.status);
+
+                // 默认状态，左滑结束时
+                if (item.status === 0 && additionalEvent === 'panleft') {
+                    if (itemLeft <= -10) {
+                        $(item).stop().animate({'left': -spanWidth + 'px'}, () => {
+                            item.status = 1;
+                        });
+                    } else {
+                        $(item).stop().animate({'left': '0px'});
+                    }
+                }
+
+                // 删除按钮打开，右滑结束时
+                if (item.status === 1 && additionalEvent === 'panright') {
+                    if (spanWidth + itemLeft >= 10) {
+                        $(item).stop().animate({'left': '0px'}, () => {
+                            item.status = 0;
+                        });
+                    } else {
+                        $(item).stop().animate({'left': -spanWidth + 'px'});
+                    }
+                }
+
+                // 默认状态，右滑结束时，确认完成动作
+                if (item.status === 0 && additionalEvent === 'panright') {
+                    if (spanWidth - itemLeft >= 10) {
+                        $(item).stop().animate({'left': '0px'});
+                    } else {
+                        $(item).stop().animate({'left': spanWidth + 'px'}, () => {
+                            item.status = 2;
+                            // todo 完成操作
+                        });
+                    }
+                }
             });
         }
     }
@@ -121,17 +181,13 @@ section {
         margin: 0;
         list-style: none;
         border-top: 1px solid #CCC; /*no*/
-        border-bottom: 1px solid #CCC; /*no*/
-
-        li + li {
-            border-top: 1px solid #CCC; /*no*/
-        }
 
         li {
             height: 100px;
             line-height: 100px;
             font-size: 40px;
             position: relative;
+            border-bottom: 1px solid #CCC; /*no*/
 
             &.finished div {
                 background-color:rgb(225, 245, 206);
@@ -146,7 +202,8 @@ section {
                 color: #FFF;
                 
                 &.finish {
-                    left: 30px;
+                    left: 0px;
+                    background-color: #67C23A;
                 }
 
                 &.delete {
